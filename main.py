ac22a1352a86780afc03427bc225b0a7231c895b
ajarpit02 <arpit.jain@techolution.com>
2025-12-05 15:05:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 print(a+b)
 a=2,b=5
+def say_hello(name):
+    return f"Hello, {name}!"
+
+def say_bye(name):
+    return f"Goodbye, {name}!"
+
+if __name__ == "__main__":
+    print(say_hello("Arpit"))
+    print(say_bye("Arpit"))
